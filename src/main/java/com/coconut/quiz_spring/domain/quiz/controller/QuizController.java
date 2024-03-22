856891b9d62f3788d64dcc466d9e68885b439ea1
devsince2021 <dev.ryu2021@gmail.com>
2024-03-22 17:14:29 +0900

    r&d: openai 연동

--- src/main/java/com/coconut/quiz_spring/domain/quiz/controller/QuizController.java
+++ src/main/java/com/coconut/quiz_spring/domain/quiz/controller/QuizController.java
@@ -1,14 +1,42 @@
 package com.coconut.quiz_spring.domain.quiz.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.coconut.quiz_spring.domain.quiz.service.OpenAiService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class QuizController {
 
-  @GetMapping("/")
-  public String getQuiz() {
-    return "hi222";
+  @Autowired
+  private OpenAiService openAiService;
+
+
+  @CrossOrigin(origins = "http://localhost:3000")
+  @PostMapping("/quiz")
+  public String generateQuiz() throws IOException, InterruptedException {
+    String result = openAiService.generateQuiz();
+    return result;
   }
+
+  @CrossOrigin(origins = "http://localhost:3000")
+  @PostMapping("/answer")
+  public String generateAnswer(@RequestBody Map<String, String> req) throws IOException, InterruptedException {
+
+    String result = openAiService.generateAnswer(req.toString());
+
+    log.info("result: >>" + result);
+    return result;
+  }
+
+
 }

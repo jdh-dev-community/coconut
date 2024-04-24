cf5d51e01555dcba2ecde1111a16ea492aa2b050
devsince2021 <dev.ryu2021@gmail.com>
2024-04-25 00:04:50 +0900

    fix: 컴포넌트 스캔 범위 수정

--- quiz_service/src/main/java/com/coconut/quiz_service/QuizApplication.java
+++ quiz_service/src/main/java/com/coconut/quiz_service/QuizApplication.java
@@ -2,11 +2,9 @@ package com.coconut.quiz_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.coconut.global", "com.coconut.quiz_service"})
+@SpringBootApplication(scanBasePackages = {"com.coconut.global", "com.coconut.quiz_service"})
 public class QuizApplication {
 
   public static void main(String[] args) {

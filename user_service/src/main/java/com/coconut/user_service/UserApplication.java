6c2d36ab53091ba4ee6f0312d38e9ab20cd9a06f
devsince2021 <dev.ryu2021@gmail.com>
2024-04-28 15:52:04 +0900

    init: 서비스 생성

---
+++ user_service/src/main/java/com/coconut/user_service/UserApplication.java
@@ -0,0 +1,11 @@
+package com.coconut.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.coconut.global", "com.coconut.user_service"})
+public class UserApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(UserApplication.class);
+  }
+}

44e9a2833b18a77732218e0affba5e18edc67ad7
devsince2021 <dev.ryu2021@gmail.com>
2024-04-30 17:20:41 +0900

    add: jpa_utils 반영

--- user_service/src/main/java/com/coconut/user_service/UserApplication.java
+++ user_service/src/main/java/com/coconut/user_service/UserApplication.java
@@ -3,7 +3,7 @@ package com.coconut.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.coconut.global", "com.coconut.user_service"})
+@SpringBootApplication(scanBasePackages = {"com.coconut.global", "com.coconut.jpa_utils", "com.coconut.user_service"})
 public class UserApplication {
   public static void main(String[] args) {
     SpringApplication.run(UserApplication.class);
--- user_service/src/main/java/com/coconut/user_service/common/advice/UserControllerAdvice.java
+++ user_service/src/main/java/com/coconut/user_service/common/advice/UserControllerAdvice.java
@@ -1,9 +1,10 @@
 package com.coconut.user_service.common.advice;
 
+import com.coconut.global.advice.BaseControllerAdvice;
+import com.coconut.global.constant.ControllerAdviceOrder;
 import com.coconut.global.dto.CustomResponse;
 import com.coconut.global.dto.HttpErrorInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.PriorityOrdered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,9 +18,9 @@ import javax.validation.UnexpectedTypeException;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Slf4j
-@Order(PriorityOrdered.LOWEST_PRECEDENCE - 1)
+@Order(ControllerAdviceOrder.SERVICE)
 @ControllerAdvice
-public class UserControllerAdvice {
+public class UserControllerAdvice extends BaseControllerAdvice {
 
   @ResponseStatus(BAD_REQUEST)
   @ExceptionHandler(UnexpectedTypeException.class)
@@ -27,15 +28,6 @@ public class UserControllerAdvice {
     log.info("error handler: handleUnexpectedTypeException");
     HttpErrorInfo errorInfo = createHttpErrorInfo(BAD_REQUEST, req, ex);
     return CustomResponse.of(errorInfo);
-  }
-
-  private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, WebRequest req, Exception ex) {
-    final String path = req.getDescription(false).replace("uri=", "");
-    final String message = ex.getMessage();
-    final String errorName = ex.getClass().getName();
-
-    log.debug("Returning HTTP status: {} for path: {}, message: {}, error name: {}", httpStatus, path, message, errorName);
-    return new HttpErrorInfo(httpStatus, path, message, errorName);
   }
 
 }

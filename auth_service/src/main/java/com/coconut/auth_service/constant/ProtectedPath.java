dbc11ba03477501091f4f8ea94fb6d43d3bb1966
devsince2021 <dev.ryu2021@gmail.com>
2024-05-23 16:12:36 +0900

    add: 인증 경로 수정

--- auth_service/src/main/java/com/coconut/auth_service/constant/ProtectedPath.java
+++ auth_service/src/main/java/com/coconut/auth_service/constant/ProtectedPath.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 public class ProtectedPath {
   private final String baseV1 = "/api/v1";
 
-  private final List<String> quizService = List.of("/quiz/jobposting", "/answer");
+  private final List<String> quizService = List.of("/validation");
 
   private final List<String> communityService = List.of();
 
